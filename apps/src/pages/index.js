4247539c9690af9bf7e22063e931a055c9ac2b60
Yixiao Chen <ychen239@uottawa.ca>
2019-08-06 21:53:39 -0400

    use classnames and active state to change tab color. add btn-light class to change inactive tab color. add logs, will pull data from Github commit api

--- apps/src/pages/index.js
+++ apps/src/pages/index.js
@@ -4,11 +4,10 @@ import {Link, StaticQuery, graphql} from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import classnames from "classnames";
-import "../assets/css/blk-design-system-react.css";
-import "../assets/css/nucleo-icons.css";
+import 'bootstrap/dist/css/bootstrap.css';
+import SiteListImg from "../images/apps-sites-list.png";
 
 
-// reactstrap components
 import {
     NavItem,
     NavLink,
@@ -19,6 +18,7 @@ import {
     Col
 } from "reactstrap";
 
+
 //https://github.com/gatsbyjs/gatsby/issues/10523#issue-391866891
 
 class Tester extends React.Component {
@@ -26,7 +26,7 @@ class Tester extends React.Component {
         super(props);
         this.state = {
             vertical: 0,
-            withIcons: 0
+            activeTab: 0
         };
     }
 
@@ -38,21 +38,25 @@ class Tester extends React.Component {
     };
 
     prepareNavs = (data, elemType) => { //helpler function to iterate over object and push elements into array
+
         const elemArray = [];
         switch (elemType) {
             case "title": {
                 data.allPostsJson.edges.forEach(item =>
                     elemArray.push(
-                        <NavItem key={(item.node.id)}>
+                        <NavItem key={(item.node.id)} style={{width: "50%"}}>
                             <NavLink
+                                style={{height: "140px"}}
                                 className={classnames({
-                                    active: this.state.withIcons === parseInt(item.node.id)
+                                    active: this.state.activeTab === parseInt(item.node.id),
+                                    "btn": true,
+                                    "btn-info": this.state.activeTab === parseInt(item.node.id),
+                                    "btn-light": ! (this.state.activeTab === parseInt(item.node.id))
                                 })}
-                                onClick={e => this.toggleTabs(e, "withIcons", parseInt(item.node.id))}
-                                href="#pablo"
-
+                                onClick={e => this.toggleTabs(e, "activeTab", parseInt(item.node.id))}
+                                href="#"
                             >
-                                {item.node.title}
+                                <p>{item.node.title}</p>
                             </NavLink>
                         </NavItem>
                     )
@@ -62,21 +66,32 @@ class Tester extends React.Component {
             case "tabpane": {
                 data.allPostsJson.edges.forEach(item =>
                     elemArray.push(
-                        <TabPane tabId={"withIcons"+parseInt(item.node.id)}>
-                            <h4 color="black">Description</h4>{item.node.description ? item.node.description : "To be added soon!"} <br /><br />
-                            <h4 color="black">Link</h4><a href={item.node.link ? item.node.link : "#"}>{item.node.link ? item.node.link : "To be added soon!"}</a> <br /><br />
-                            <h4 color="black">Type</h4>{item.node.type ? item.node.type : "To be added soon!"} <br /><br />
-                            <h4 color="black">Implementation Method:</h4>{item.node.implementation? item.node.implementation : "To be added soon!"} <br /><br />
-                            <h4 color="black">Source Code Location</h4>{item.node.source_code ? item.node.source_code : "To be added soon!"} <br /><br />
-                            <h4 color="black">Current Status and Plan</h4>{item.node.plan ? item.node.plan : "To be added soon!"} <br /><br />
-                            <h4 color="black">Change Log</h4>{item.node.source_code ? item.node.source_code : "To be added soon!"} <br /><br />
+                        <TabPane key={(item.node.id)} tabId={parseInt(item.node.id)}>
+
+                            <img src={item.node.img} />
+                            <h4 color="black">Description</h4>{item.node.description ? item.node.description : "To be added soon!"}
+                            <br/><br/>
+                            <h4 color="black">Link</h4><a
+                            href={item.node.link ? item.node.link : "#"}>{item.node.link ? item.node.link : "To be added soon!"}</a>
+                            <br/><br/>
+                            <h4 color="black">Type</h4>{item.node.type ? item.node.type : "To be added soon!"}
+                            <br/><br/>
+                            <h4 color="black">Implementation
+                                Method:</h4>{item.node.implementation ? item.node.implementation : "To be added soon!"}
+                            <br/><br/>
+                            <h4 color="black">Source Code
+                                Location</h4>{item.node.source_code ? item.node.source_code : "To be added soon!"} <br/><br/>
+                            <h4 color="black">Current Status and
+                                Plan</h4>{item.node.plan ? item.node.plan : "To be added soon!"} <br/><br/>
+                            <h4 color="black">Change
+                                Log</h4>{item.node.show_logs ? item.node.show_logs : "To be added soon!"} <br/><br/>
                         </TabPane>
                     )
                 );
                 break;
             }
             default :
-                console.log("can not create element type - "+elemType);
+                console.log("can not create element type - " + elemType);
                 break;
         }
 
@@ -99,24 +114,27 @@ class Tester extends React.Component {
                             implementation
                             plan
                             source_code
+                            img
+                            show_logs
                           }
                         }
                       }
                     }`
                 }
                 render={data => (
-                    <Layout>
+                    <Layout>{console.log(data)}
                         <SEO title="PianoLab Engineering Projects"/>
                         <>
                             <Row>
-                                <Col md="4">
-                                    <Nav className="nav-pills-primary nav-pills-icons flex-column" pills>
+                                <Col md="6">
+                                    <Nav pills>
                                         {this.prepareNavs(data, "title")}
                                     </Nav>
                                 </Col>
-                                <Col md="8">
-                                    <TabContent className="tab-space" activeTab={"withIcons" + this.state.withIcons}>
-                                        {this.prepareNavs(data,"tabpane")}
+
+                                <Col md="6" className="d-none d-md-block">
+                                    <TabContent activeTab={this.state.activeTab}>
+                                        {this.prepareNavs(data, "tabpane")}
                                     </TabContent>
                                 </Col>
                             </Row>
@@ -135,3 +153,6 @@ export default Tester;
 //https://itnext.io/reading-data-from-a-json-file-with-gatsby-graphql-572b18ab98a
 
 //Pipeline: GraphQL query => get data => send into component as props => Loop throught data object to create elements => Render
+
+//https://stackoverflow.com/questions/52574783/gatsby-image-path-from-json
+//https://www.gatsbyjs.org/packages/gatsby-image/#two-types-of-responsive-images
